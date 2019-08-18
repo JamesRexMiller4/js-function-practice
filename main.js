3d056972220af6ca598ded5d60a65639eb84f3d9
James Miller <james.rex.miller.4@gmail.com>
2019-08-18 10:17:28 -0600

    JS Functions Practice - OP Fundamentals Part 3

---
+++ main.js
@@ -0,0 +1,30 @@
+// function add7(x) {
+//     console.log( x + 7);
+// }
+
+// add7 (4);
+
+// function multiply(x, y) {
+//     var mult = x * y;
+//     console.log(mult);
+// }
+
+// multiply(4, 5);
+
+// function captialize(string){
+//     var cap1= (string[0].toUpperCase());
+//     slicestring = string.slice(1, );
+//     newstring = cap1 + slicestring;
+//     console.log(newstring);
+    
+    
+// }
+
+// captialize("bumpkins");
+
+function lastLetter(string) {
+    var indexpos = string.length;
+    console.log(string[indexpos -1]);
+}
+
+lastLetter("Mr. President");
